806597c4ea3d41e673aeb37348d8c34b814c79f0
kilowatty <74945640+kilowatty@users.noreply.github.com>
2022-11-19 01:31:23 -0500

    Update

---
+++ Spark_API/Spark2/Controller/SignUpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SignUpViewController.swift
+//  Spark2
+//
+//  Created by Jessica Kent on 11/13/22.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func SignBackButton(_ sender: Any) {
+    }
+    
+    @IBAction func SignUpButton(_ sender: Any) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
